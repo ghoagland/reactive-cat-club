42a4c6f0e9585893029e19932e980f8953b46bab
Georgina Hoagland <georgina.hoagland@gmail.com>
2017-10-17 18:36:40 -0400

    actually serving index and bundle!!!!!!!!

---
+++ client/Main.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
+
+
+export default class Main extends Component {
+  render() {
+    return (
+      <div>Hello</div>
+    )
+  }
+}
+
+// export default function Main () {
+//   return (
+//     <div>Hi</div>
+//   )
+// }
--- client/index.js
+++ client/index.js
@@ -1,3 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
+import Main from './Main.jsx';
+
+ReactDOM.render(
+  <Router>
+     <Main />
+  </Router>,
+  document.getElementById('app')
+);
+
