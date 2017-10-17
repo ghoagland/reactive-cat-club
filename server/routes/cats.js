8f070ea433da9ef14d7863489a69d29f8990ec7b
Georgina Hoagland <georgina.hoagland@gmail.com>
2017-10-17 17:07:45 -0400

    added in some more routes

--- server/routes/cats.js
+++ server/routes/cats.js
@@ -3,15 +3,6 @@ const { Cat } = require('../models');
 
 module.exports = router;
 
-//get one specific cat
-router.get('/cat/:catId', (req, res, next) => {
-  Cat.findById(req.params.catId)
-  .then((cat) => {
-    res.json(cat)
-  })
-  .catch(next);
-})
-
 //get ALL the cats!
 router.get('/', (req, res, next) => {
   Cat.findAll({})
@@ -21,11 +12,33 @@ router.get('/', (req, res, next) => {
   .catch(next);
 })
 
-router.post('/cat', (req, res, next) => {
+//add a NEW cat
+router.post('/new-cat', (req, res, next) => {
   Cat.create(req.body)
   .then((newCat) => {
     res.status(201).json(newCat)
   })
   .catch(next);
 })
+
+//get one specific cat
+router.get('/:catId', (req, res, next) => {
+  Cat.findById(req.params.catId)
+  .then((cat) => {
+    res.json(cat)
+  })
+  .catch(next);
+})
+
+router.put('/:catId', (req, res, next) => {
+  Cat.findById(req.params.catId)
+  .then((cat) => {
+    return cat.update(req.body)
+  })
+  .then((updatedCat) => {
+    res.json(updatedCat);
+  })
+  .catch(next);
+})
+
 
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,3 +1,59 @@
 const router = require('express').Router();
+const { Cat, User } = require('../models')
 
-module.exports = router;
+module.exports = router
+
+//see all users
+
+router.get('/', (req, res, next) =>   {
+  User.findAll({})
+  .then((users) => {
+    res.json(users)
+  })
+  .catch(next);
+})
+
+//create a user
+router.post('/new-user', (req, res, next) => {
+  User.create(req.body)
+  .then((newUser) => {
+    res.json(newUser)
+  })
+  .catch(next);
+})
+
+//get a user
+router.get('/:userId', (req, res, next) => {
+  User.findById(req.params.userId)
+  .then((user) => {
+    res.json(user)
+  })
+  .catch(next);
+})
+
+//update a user
+router.put('/:userId', (req, res, next) => {
+  User.findById(req.params.userId)
+  .then((user) => {
+    return user.update(req.body)
+  })
+  .then((updatedUser) => {
+    res.json(updatedUser);
+  })
+  .catch(next);
+})
+
+
+
+//delete a user
+
+router.delete('/:userId', (req, res, next) => {
+  User.findById(req.params.userId)
+  .then((user) => {
+    return user.destroy();
+  })
+  .then((deletedUser) => {
+    res.json(deletedUser)
+  })
+  .catch(next);
+})
