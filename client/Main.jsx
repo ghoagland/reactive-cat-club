2b108f1a17c878ac303ee92aa301b677350dd74f
Leigh Steiner <steinerleigh@gmail.com>
2017-10-18 18:34:21 -0400

    weird server bug!

--- client/Main.jsx
+++ client/Main.jsx
@@ -4,7 +4,7 @@ import { Route, Switch } from 'react-router-dom';
 import AllCats from './AllCats.jsx';
 import SingleCat from './SingleCat.jsx';
 import SingleUser from './SingleUser.jsx';
-
+ //<Route path='/cat/:catId' component={SingleCat} />
 
 export default class Main extends Component {
   render() {
@@ -13,8 +13,7 @@ export default class Main extends Component {
       <h1>Welcome To Cat Club!</h1>
       <Switch>
         <Route path='/all-cats' component={AllCats} />
-        {/*<Route path='/cat/:catId' component={SingleCat} />
-                <Route path='/user/:userId' component={SingleUser} />*/}
+  
         <Route component={AllCats} />
       </Switch>
      </div>
---
+++ client/SingleCat.jsx
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import store, { fetchCat } from './store';
+
+export default class SingleCat extends Component {
+  constructor() {
+  	super();
+  	this.state = store.getState();
+  }
+
+  componentDidMount() {
+  	//teachable moment -- why is naming this fetchCat bad?
+  	const thunk = fetchCat(this.props.match.params.catId);
+  	store.dispatch(thunk);
+  	this.unsubscribe = store.subscribe(() => this.setState(store.getState()));
+  }
+
+  componentWillUnmount() {
+  	this.unsubscribe();
+  }
+
+  render() {
+  	const cat = this.state.singleCat;
+    console.log('cat', cat);
+     return(
+      <div className="single-cat">
+        <h1>{cat.name}</h1>
+         <ul>
+           <li>{cat.ageInYears} old</li>
+           <li>{cat.breed}</li>
+         </ul>
+         <p>{cat.description}</p>
+       </div>
+     	) 
+      
+  }
+}
