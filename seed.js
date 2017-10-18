ebcaefbf0d32954c8cc0c599f81ee4dfd7dd6afd
Georgina Hoagland <georgina.hoagland@gmail.com>
2017-10-18 10:34:44 -0400

    updated for seed

--- seed.js
+++ seed.js
@@ -3,106 +3,114 @@ const { Cat, User, db } = require('./server/models');
 
 const users = [
   {
-  	name: 'Friday Jones', 
+  	name: 'Friday Jones',
   	email: 'marjoriebaldwin@botanybay.gov'
-  }, 
+  },
   {
-  	name: 'Lilith Iyapo', 
+  	name: 'Lilith Iyapo',
   	email: 'mom@newearth.edu'
-  }, 
+  },
   {
-  	name: 'Sophie Bangs', 
+  	name: 'Sophie Bangs',
   	email: 'promethea@weepinggorilla.com'
-  }, 
+  },
   {
-  	name: 'Meg Murry', 
+  	name: 'Meg Murry',
   	email: 'tesseraction@camazotz.net'
-  }, 
+  },
   {
-  	name: 'Halo Jones', 
+  	name: 'Halo Jones',
   	email: 'piratequeen@thecrunch.io'
-  }, 
+  },
   {
-  	name: 'Jillian Boardman', 
+  	name: 'Jillian Boardman',
   	email: 'materdeum@thenest.net'
-  }, 
+  },
   {
-  	name: 'Helen Justineau', 
+  	name: 'Helen Justineau',
   	email: 'history@hotelecho.gov'
   }
 ]
-   
+
 const cats = [
   {
-  	name: 'Ellis', 
-  	ageMonths: 39, 
-  	breed: 'domestic short hair', 
-  	hasShots: true, 
-  	description: 'Ellis is an independent tuxedo who likes climbing and butter.', 
-  	UserId: 1
+  	name: 'Ellis',
+  	ageMonths: 39,
+  	breed: 'domestic short hair',
+  	hasShots: true,
+  	description: 'Ellis is an independent tuxedo who likes climbing and butter.',
+  	userId: 1
   },
    {
-  	name: 'Sol', 
-  	ageMonths: 14, 
-  	breed: 'domestic short hair', 
-  	hasShots: true, 
+  	name: 'Sol',
+  	ageMonths: 14,
+  	breed: 'domestic short hair',
+  	hasShots: true,
   	description: 'Sol is gray and loves brushing and biting, especially at the same time.',
-  	UserId: 4
-  }, 
-   {
-  	name: 'Bento', 
-  	ageMonths: 62, 
-  	breed: 'sphinx', 
-  	hasShots: true, 
-  	description: 'Bento is a ginger cat who will chase ALL the birds and loves yelling.', 
-  	UserId: 3
+  	userId: 4
   },
    {
-  	name: 'Thomas', 
-  	ageMonths: 75, 
-  	breed: 'bengal', 
-  	hasShots: true, 
-  	description: 'Thomas is a stately gentleman and a beverage criminal.', 
-  	UserId: 1
-  }, 
-   {
-  	name: 'Roxie', 
-  	ageMonths: 8, 
-  	breed: 'siamese', 
-  	hasShots: false, 
-  	description: 'If Roxie is not already in your lap, she wants to be.', 
-  	UserId: 2
+  	name: 'Bento',
+  	ageMonths: 62,
+  	breed: 'sphinx',
+  	hasShots: true,
+  	description: 'Bento is a ginger cat who will chase ALL the birds and loves yelling.',
+  	userId: 3
   },
    {
-  	name: 'Ampersand', 
-  	ageMonths: 22, 
-  	breed: 'bengal', 
-  	hasShots: true, 
-  	description: 'Ampersand is happiest when plotting with his brother, Interrobang.', 
-  	UserId: 7
-  }, 
-  {
-  	name: 'Interrobang', 
-  	ageMonths: 22, 
-  	breed: 'bengal', 
-  	hasShots: true, 
-  	description: 'Interrobang is happiest when plotting with his brother, Ampersand.', 
-  	UserId: 7
+  	name: 'Thomas',
+  	ageMonths: 75,
+  	breed: 'bengal',
+  	hasShots: true,
+  	description: 'Thomas is a stately gentleman and a beverage criminal.',
+  	userId: 1
+  },
+   {
+  	name: 'Roxie',
+  	ageMonths: 8,
+  	breed: 'siamese',
+  	hasShots: false,
+  	description: 'If Roxie is not already in your lap, she wants to be.',
+  	userId: 2
+  },
+   {
+  	name: 'Ampersand',
+  	ageMonths: 22,
+  	breed: 'bengal',
+  	hasShots: true,
+  	description: 'Ampersand is happiest when plotting with his brother, Interrobang.',
+  	userId: 7
   },
   {
-  	name: 'M.O.M.A', 
-  	ageMonths: 43, 
-  	breed: 'domestic short hair', 
-  	hasShots: false, 
-  	description: 'M.O.M.A likes dogs. For breakfast.', 
-  	UserId: 5
+  	name: 'Interrobang',
+  	ageMonths: 22,
+  	breed: 'bengal',
+  	hasShots: true,
+  	description: 'Interrobang is happiest when plotting with his brother, Ampersand.',
+  	userId: 7
+  },
+  {
+  	name: 'M.O.M.A',
+  	ageMonths: 43,
+  	breed: 'domestic short hair',
+  	hasShots: false,
+  	description: 'M.O.M.A likes dogs. For breakfast.',
+  	userId: 5
+  },
+  {
+    name: 'Pammy',
+    age: 76,
+    breed: 'domestic short hair',
+    hasShots: true,
+    description: 'Do you have turkey?',
+    userId: 4
   }
 ]
 
 
-const seed = () => 
+const seed = () =>
 	Promise.all(users.map( user => User.create(user)))
-	.then(() => 
+	.then(() =>
 	Promise.all(cats.map( cat => Cat.create(cat))));
 
 const main = () => {
